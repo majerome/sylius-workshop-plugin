4c62b146aad8aa11e186799c559bd3194f7e9a0f
jelegrand <jelegrand@sqli.com>
2025-04-16 18:27:11 +0200

    Add Majerome Workshop Plugin Validations

---
+++ src/Validator/Constraints/BrandHasCorrectType.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Majerome\WorkshopPlugin\Validator\Constraints;
+
+use Majerome\WorkshopPlugin\Validator\BrandHasCorrectTypeValidator;
+use Symfony\Component\Validator\Constraint;
+
+final class BrandHasCorrectType extends Constraint
+{
+    public string $message = 'majerome_workshop_plugin.brand.type.invalid';
+
+    public function validatedBy(): string
+    {
+        return BrandHasCorrectTypeValidator::class;
+    }
+
+    public function getTargets(): string
+    {
+        return self::CLASS_CONSTRAINT;
+    }
+}
