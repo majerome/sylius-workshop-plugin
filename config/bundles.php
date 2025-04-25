fe26af96068fa58319fa74060a75f3989d7d277c
jelegrand <jelegrand@sqli.com>
2025-04-25 10:18:04 +0200

    Upgrade Sylius from 1.14 to 2.0

---
+++ config/bundles.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
+    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
+    Symfony\UX\LiveComponent\LiveComponentBundle::class => ['all' => true],
+    Symfony\UX\Icons\UXIconsBundle::class => ['all' => true],
+    Symfony\UX\Autocomplete\AutocompleteBundle::class => ['all' => true],
+    ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+];
